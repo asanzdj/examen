b79edd0b48a7809a7797cdd76d2d2848c79af911
Andrea Sanz <asanzdj@gmail.com>
2017-02-23 16:00:09 +0100

    [client] & [server] Blocked date after current date in question create

--- server/src/question/create.js
+++ server/src/question/create.js
@@ -10,6 +10,7 @@ export default (app) => {
   app.post('/api/question', passport.authenticate('jwt', {session: false}), asyncRequest(async (req, res) => {
     // get user input
     const {text, expirationDate} = req.body;
+    const today = moment().format('YYYY-MM-DD');
 
     // make sure text is not empty
     if (!text || !text.length) {
@@ -20,6 +21,11 @@ export default (app) => {
     // validate date
     if (!moment(expirationDate, moment.ISO_8601).isValid()) {
       res.status(400).send({error: 'Date should be valid ISO Date!'});
+      return;
+    }
+
+    if (expirationDate < today) {
+      res.status(400).send({error: 'Date should be after current date!'});
       return;
     }
 
